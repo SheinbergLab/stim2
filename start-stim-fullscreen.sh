de0dcd725557590251c3cc7bf3946d2c578b6f2f
David Sheinberg <sheinb@up7000-dev.neuro.brown.edu>
2024-07-13 11:32:50 -0400

    adjust path to executable and default config

--- start-stim-fullscreen.sh
+++ start-stim-fullscreen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /shared/qpcs/projects/stimulator2
 export DLSH_LIBRARY=/usr/local/lib/tcltk/dlsh
-/usr/local/bin/stim2 -f /usr/local/share/stim2/config/linux.cfg -F -r 100
+/usr/local/stim2/stim2 -f /usr/local/stim2/config/linux.cfg -F -r 100
