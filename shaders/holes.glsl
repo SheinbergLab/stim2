d98c10d24bd893b3988e3c6ecbac542e75cd44d8
David Sheinberg <David_Sheinberg@brown.edu>
2025-05-06 10:53:59 -0400

    added rotation

--- shaders/holes.glsl
+++ shaders/holes.glsl
@@ -16,35 +16,52 @@ void main(void)
 
 -- Fragment
 
+#define MAX_CIRCLES 20
 uniform float time;
 uniform vec2 resolution;
 
 uniform float radius;        // Radius of each circle
-uniform float smoothness;    // Edge smoothness (0.0 = hard edge, higher = softer)
+uniform float smoothness;    // Edge smoothness (0.0 = hard, higher = softer)
 uniform vec4  maskColor;     // RGBA color of the mask
 
-uniform sampler2D tex0;
-uniform vec2 circlepos;
+uniform int nCircles;
+uniform float rotationAngle;
+uniform vec2 circlePos[MAX_CIRCLES];
 
+uniform sampler2D tex0;
 in vec2 texcoord;
 out vec4 fragcolor;
 
+vec2 rotate(vec2 point, float angle) {
+    float s = sin(radians(angle));
+    float c = cos(radians(angle));
+
+    return vec2(
+        point.x * c - point.y * s,
+        point.x * s + point.y * c
+    );
+}
+
 float drawCircle(vec2 position, vec2 center, float radius, float smoothness) {
-    float dist = distance(position, center);
+    float dist = distance(position, rotate(center, rotationAngle));
+
     // 1.0 at the center of the circle, 0.0 outside the circle
     return 1.0 - smoothstep(radius - smoothness, radius + smoothness, dist);
 }
 
 void main(void){
-    // Scale to pixel coordinates for circle calculations
-    vec2 position = gl_FragCoord.xy;
-    
+    vec2 position = vec2(texcoord.s-.5, texcoord.y-0.5);
+
     // Start with no holes (solid mask)
     float mask = 0.0;
 
-    // Add this circle's contribution to the mask
-    mask = max(mask, drawCircle(position, circlepos, radius, smoothness));
-
+    // Add each circle's contribution to the mask
+    for (int i = 0; i < MAX_CIRCLES; i++) {
+       if (i >= nCircles) break;
+       mask = max(mask,
+                  drawCircle(position, circlePos[i], radius, smoothness));
+    }
+    
     vec4 textureColor = texture(tex0, vec2(texcoord.s, 1.0-texcoord.t));
 
     fragcolor = mix(maskColor, textureColor, mask);
@@ -52,7 +69,9 @@ void main(void){
 
 -- Uniforms
 
-radius 40
-smoothness 2
-maskColor "0.2 0.1 0.9 1.0"
-circlepos "0.0 0.0"
+radius 0.1
+smoothness .005
+maskColor "0.6 0.1 0.9 1.0"
+circlePos "-.1 0.0 0.0 .2"
+nCircles 2
+rotationAngle 0
