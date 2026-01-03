5193de9218796df70fa4347021c31e040877dfd1
David Sheinberg <David_Sheinberg@brown.edu>
2026-01-03 00:25:27 -0500

    update rectangle example to use adjusters

--- examples/polygon/rectangle.tcl
+++ examples/polygon/rectangle.tcl
@@ -1,25 +1,56 @@
 # examples/basic/rectangle.tcl
-workspace::reset
 
-proc rectangle {width height r g b} {
+proc rectangle { { width 4 } { height 4 } } {
     glistInit 1
     resetObjList
 
     set s [polygon]
-    scaleObj $s $width $height
-    polycolor $s $r $g $b
+    objName $s rect
+    scaleObj rect $width $height
     
-    glistAddObject $s 0
+    glistAddObject rect 0
 
     glistSetVisible 1
     glistSetCurGroup 0
     redraw
 }
 
-workspace::export rectangle {
-    width  {float 0.1 10 0.1 4.0 "Width" deg}
-    height {float 0.1 10 0.1 4.0 "Height" deg}
-    r      {float 0 1 0.05 0.5 "Red"}
-    g      {float 0 1 0.05 0.5 "Green"}
-    b      {float 0 1 0.05 1.0 "Blue"}
+proc set_size { name w h } {
+    scaleObj $name $w $h
+    redraw
 }
+
+proc set_rotation { name angle } {
+    rotateObj $name $angle 0 0 1
+    redraw
+}
+
+proc set_color { name r g b } {
+    polycolor $name $r $g $b
+    redraw
+}
+
+
+#######################################################################
+##                         Workspace Setup                           ##
+#######################################################################
+
+workspace::reset
+
+workspace::setup rectangle {
+} -adjusters {rect_size rect_rotation rect_color}
+
+workspace::adjuster rect_size {
+    width  {float 0.2 10.0 0.1 4.0 "Width" deg}
+    height {float 0.2 10.0 0.1 4.0 "Height" deg}
+} -target rect -proc set_size
+
+workspace::adjuster rect_rotation {
+    angle  {float 0 360 1 0 "Angle" deg}
+} -target rect -proc set_rotation
+
+workspace::adjuster rect_color {
+    r      {float 0 1 0.05 1.0 "Red"}
+    g      {float 0 1 0.05 1.0 "Green"}
+    b      {float 0 1 0.05 1.0 "Blue"}
+} -target rect -proc set_color
