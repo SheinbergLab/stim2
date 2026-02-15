8870d86fb76ab476ce9b7fe1f31b6a7a9486dd85
David Sheinberg <David_Sheinberg@brown.edu>
2026-02-15 00:24:01 -0500

    iniital commit

---
+++ assets/world/PinkStar.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="PinkStar" tilewidth="32" tileheight="32" tilecount="39" columns="39">
+ <properties>
+  <property name="aseprite_json" value="PinkStar.json"/>
+ </properties>
+ <image source="PinkStar.png" width="1248" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.36887" y="6.75937" width="21.2248" height="24.2781"/>
+  </objectgroup>
+ </tile>
+</tileset>
